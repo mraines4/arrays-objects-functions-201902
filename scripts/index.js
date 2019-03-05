23392fc7bc0e6ec87ed5428c4d140f73c936b608
Matt Raines <mraines4@gmail.com>
2019-03-05 14:19:43 -0500

    adds call3Times function

--- scripts/index.js
+++ scripts/index.js
@@ -124,3 +124,25 @@ function goodJob(name) {
 }
 
 goodJob(people);
+
+// 3 times
+// Given this function:
+
+// function call3Times(fun) {
+//     fun();
+//     fun();
+//     fun();
+// }
+// Use the call3Times function to print "Hello, world!" 3 times.
+
+function call3Times(fun) {
+    fun();
+    fun();
+    fun();
+}
+
+function helloWorld() {
+    console.log('Hello World!')
+}
+
+call3Times(helloWorld);
