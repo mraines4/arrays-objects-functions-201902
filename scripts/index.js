ab385b4bfb569e38fd29b26cdef5a29ab6a3a57f
Matt Raines <mraines4@gmail.com>
2019-03-05 13:37:08 -0500

    adds positiveNumber function

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,13 @@
 // add your solutions here
+
+// Positive Numbers
+// Write a function which takes an array of numbers as input and returns a new array containing only the positive numbers in the given array.
+
+function positiveNumbers(arr) {
+    const newArray = arr.filter(function (num) {
+        return num > 0;
+    });
+    return newArray;
+}
+
+console.log(positiveNumbers([1, -4, 5, -6]));
