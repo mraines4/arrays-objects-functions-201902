983f56a4df2732ff126a9a9b47e0642aae7dd80f
Matt Raines <mraines4@gmail.com>
2019-03-05 14:08:21 -0500

    adds coldCities

--- scripts/index.js
+++ scripts/index.js
@@ -36,3 +36,32 @@ function squaredNumbers(arr) {
 }
 
 console.log(squaredNumbers([1, 2, 3]));
+
+// Cities 1
+// Write a function which takes an array of city objects like such:
+
+// var cities = [
+//     { name: 'Los Angeles', temperature: 60.0},
+//     { name: 'Atlanta', temperature: 52.0 },
+//     { name: 'Detroit', temperature: 48.0 },
+//     { name: 'New York', temperature: 80.0 }
+// ];
+// as input and returns a new array containing the cities whose temperature is cooler than 70 degrees.
+
+const cities = [
+    { name: 'Los Angeles', temperature: 60.0},
+    { name: 'Atlanta', temperature: 52.0 },
+    { name: 'Detroit', temperature: 48.0 },
+    { name: 'New York', temperature: 80.0 }
+];
+
+function coldCities(place) {
+    let coldPlace = cities.filter(function (which) {
+        return which.temperature < 70.0;
+    });
+    let coldName = coldPlace.map(function (each) {
+        return each.name;
+    });
+    return coldName;
+}
+console.log(coldCities(cities))
