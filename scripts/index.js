e6177b851b14524c5649ec2a0312e482a07e3068
Matt Raines <mraines4@gmail.com>
2019-03-05 14:52:05 -0500

    adds lowest to highest by price

--- scripts/index.js
+++ scripts/index.js
@@ -208,3 +208,30 @@ function shortestFirst(name) {
     return shortToLong;
 }
 console.log(shortestFirst(people))
+
+// Sort an array, 3
+// Given an array of array of products:
+
+// var products = [
+//     { name: 'Basketball', price: 12.00 },
+//     { name: 'Tennis Racquet', price: 66.00 },
+//     { name: 'Tennis Balls', price: 9.00 },
+//     { name: 'Tennis Balls', price: 9.00 }
+// ];
+// Sort the array by price.
+
+var products = [
+    { name: 'Basketball', price: 12.00 },
+    { name: 'Tennis Racquet', price: 66.00 },
+    { name: 'Tennis Balls', price: 9.00 },
+    { name: 'Tennis Balls', price: 9.00 }
+];
+
+function lowestToHighest(arr) {
+    arr.sort(function (a, b) {
+        return a.price - b.price;
+    })
+    return arr;
+}
+
+console.log(lowestToHighest(products));
