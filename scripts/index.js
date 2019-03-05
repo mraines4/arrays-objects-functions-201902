3abc76e70df6ba88e3abcde1157335013857d21a
Matt Raines <mraines4@gmail.com>
2019-03-05 14:22:30 -0500

    adds callNTimes function

--- scripts/index.js
+++ scripts/index.js
@@ -146,3 +146,22 @@ function helloWorld() {
 }
 
 call3Times(helloWorld);
+
+// n times
+// You will write a function callNTimes that takes two arguments: times as a number, and fun as a function. It will call that function for that many times. Example:
+
+// > callNTimes(5, hello)
+// Hello, world!
+// Hello, world!
+// Hello, world!
+// Hello, world!
+// Hello, world!
+// You are allowed to use a loop in the implementation of callNTimes.
+
+function callNTimes(time, fun) {
+    for (let i = 0; i < time; i++) {
+        fun();
+    }
+}
+
+callNTimes(5, helloWorld);
