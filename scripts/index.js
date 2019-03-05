30c9864bef5c6bcb77f7872dde16e0e6accdb057
Matt Raines <mraines4@gmail.com>
2019-03-05 14:16:42 -0500

    adds goodJob function

--- scripts/index.js
+++ scripts/index.js
@@ -76,3 +76,51 @@ function cityNames(place) {
     return name;
 }
 console.log(cityNames(cities));
+
+// Good Job!
+// Given an array of people's names:
+
+// var people = [
+//     'Dom',
+//     'Lyn',
+//     'Kirk',
+//     'Autumn',
+//     'Trista',
+//     'Jesslyn',
+//     'Kevin',
+//     'John',
+//     'Eli',
+//     'Juan',
+//     'Robert',
+//     'Keyur',
+//     'Jason',
+//     'Che',
+//     'Ben'
+// ];
+// Print out 'Good Job, {{name}}!' for each person's name, one on a line.
+
+var people = [
+    'Dom',
+    'Lyn',
+    'Kirk',
+    'Autumn',
+    'Trista',
+    'Jesslyn',
+    'Kevin',
+    'John',
+    'Eli',
+    'Juan',
+    'Robert',
+    'Keyur',
+    'Jason',
+    'Che',
+    'Ben'
+];
+
+function goodJob(name) {
+    name.forEach(function (each) {
+        console.log(`Good Job ${each}!`);
+    });
+}
+
+goodJob(people);
