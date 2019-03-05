c75f934a4029f0485dc47ca0917749e89f46ae9e
Matt Raines <mraines4@gmail.com>
2019-03-05 13:42:09 -0500

    adds function to filter out evenNumbers

--- scripts/index.js
+++ scripts/index.js
@@ -11,3 +11,15 @@ function positiveNumbers(arr) {
 }
 
 console.log(positiveNumbers([1, -4, 5, -6]));
+
+// Even Numbers
+// Write a function which takes an array of numbers as input and returns a new array containing only the even numbers in the given array.
+
+function evenNumbers(arr) {
+    const evenArray = arr.filter(function (num) {
+        return (num % 2 === 0);
+    });
+    return evenArray;
+}
+
+console.log(evenNumbers([1, 4, 5, 6, 7]));
