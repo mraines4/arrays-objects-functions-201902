0d18b571eaf297e1219563cb9fe850ac794affe4
Matt Raines <mraines4@gmail.com>
2019-03-05 13:45:01 -0500

    adds function to squaredNumbers

--- scripts/index.js
+++ scripts/index.js
@@ -23,3 +23,16 @@ function evenNumbers(arr) {
 }
 
 console.log(evenNumbers([1, 4, 5, 6, 7]));
+
+// Square the Numbers
+// Write a function which takes an array of numbers as input and returns a new array containing result of squaring each of the numbers in the given array by two. Example: squareTheNumbers([1, 2, 3]) should give [1, 4, 9].
+
+function squaredNumbers(arr) {
+    const squaredArray = arr.map(function (num) {
+        return num * num
+    });
+
+    return squaredArray;
+}
+
+console.log(squaredNumbers([1, 2, 3]));
